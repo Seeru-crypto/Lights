cb24e64227719d4856b67bcc03b25974ac3f25fe
Seeru <puupuha@gmail.com>
2025-03-02 12:19:27 +0200

    feat: add generator content WIP

--- UI/src/components/Generator/Generator.tsx
+++ UI/src/components/Generator/Generator.tsx
@@ -1,8 +1,30 @@
 import styles from "./Generator.module.scss"
+import {Button} from "antd";
+import {useState} from "react";
 
 const Generator = () => {
+    const [result, setResult] = useState("");
+    const [maxCharacters, setMaxcharacters] = useState("");
+    // first select max characters and then a random string is generated according to that length
+
+    function generate() {
+
+        setResult("res")
+    }
+
     return (
-        <h2>Generator</h2>
+        <div className={styles.container}>
+            <h2>Generator</h2>
+            <div className={styles.results}>
+                <p>Result</p>
+                <p>{result}</p>
+            </div>
+
+
+            <Button onClick={() => generate()}>Generate</Button>
+
+
+        </div>
     )
 
 }
